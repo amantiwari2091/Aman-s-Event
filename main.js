b9bc3162dd8a02f3ac76a05e18769fb49d726ca3
amantiwari2091 <62899402+amantiwari2091@users.noreply.github.com>
2024-06-18 23:31:29 +0530

    Portfolio

---
+++ main.js
@@ -0,0 +1,63 @@
+const dynamicContent = document.getElementById("dynamictext");
+
+const phrases = ["Microsoft Learn Student Ambassador", "Google DSC Lead'23", "Developer", ""]
+let pharseIndex = 0;
+let letterIndex = 0;
+const typingSpeed = 150;
+const erasingSpeed = 100;
+
+function printLetters(phrase) {
+
+    if (letterIndex == phrase.length) {
+        // clear letter which have been typed
+        clearLetters();
+    }
+    else if (letterIndex < phrase.length) {
+        dynamicContent.textContent += phrase.charAt(letterIndex);
+        letterIndex += 1;
+        setTimeout(function () {
+            printLetters(phrase)
+        }, typingSpeed)
+    }
+}
+
+function clearLetters() {
+    if (letterIndex == -1) {
+        pharseIndex = (pharseIndex + 1) % phrases.length;
+        letterIndex = 0;
+        printLetters(phrases[pharseIndex])
+    }
+    else if (letterIndex > -1) {
+        let updatedPhrase = "";
+        for (let index = 0; index < letterIndex; index++) {
+            updatedPhrase += phrases[pharseIndex].charAt(index);
+
+        }
+        dynamicContent.textContent = updatedPhrase;
+        letterIndex -= 1;
+        setTimeout(clearLetters, erasingSpeed)
+    }
+}
+
+printLetters(phrases[pharseIndex])
+
+
+
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
